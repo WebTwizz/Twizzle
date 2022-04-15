a0b89df541406c7fc2a6a1852bae5ca7b01783d2
ahmedwab <a.abdelfattah@vehikl.com>
2022-04-15 18:01:49 -0400

    Create Slider Input Component

---
+++ src/components/Inputs/Slider/Slider.stories.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Slider } from './Slider';
+
+
+export default {
+    title: 'Components/Inputs/Slider',
+    component: Slider,
+} as ComponentMeta<typeof Slider>;
+
+export const Default: ComponentStory<typeof Slider> = () => { 
+    const [value, setValue] = useState(0);
+    const changeSlider= (num: number) => {
+        setValue(num);
+    }
+    return ( <> 
+                <h1>Value: {value}</h1>
+                <Slider range={[1, 100]} position={value} onChange={(e) => changeSlider(e)}/> 
+            </> ) 
+            }
