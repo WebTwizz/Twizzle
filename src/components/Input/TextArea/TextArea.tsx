680910c578bc060a005d61bd7fa9b974c7af332c
ahmedwab <a.abdelfattah@vehikl.com>
2022-04-16 04:57:27 -0400

    Create TextArea Component

--- src/components/Input/TextArea/TextArea.tsx
+++ src/components/Input/TextArea/TextArea.tsx
@@ -1,5 +1,3 @@
-//create a simple input text box
-
 import { useContext, useState } from "react";
 import { ThemeContext } from "../../../context/ThemeContext";
 import { InputProps } from "../Input/Input";
@@ -24,30 +22,47 @@ export const TextArea: React.FC<TextAreaProps> = ({
   const theme = useContext(ThemeContext);
   return (
     <div
-      className="twizzle-input-textarea-container"
-      style={{
-        border: `0.5px solid ${
-          hover ? theme.primary?.backgroundColor : "#E8E8E8"
-        }`,
-        backgroundColor: disabled ? "#e9e9e9" : "white",
-        cursor: disabled ? "not-allowed" : "text",
-        ...style,
-      }}
+    className="twizzle-input-textarea-container"
+    style={{
+      
+      backgroundColor: disabled ? "#e9e9e9" : "white",
+      cursor: disabled ? "not-allowed" : "text",
+      ...style,
+    }}
     >
-      <TextArea
-        placeholder={placeholder}
+      <textarea
+      className="twizzle-input-textarea"
+      placeholder={placeholder}
+      style={{
+       border: `0.5px solid ${  hover ? theme.primary?.backgroundColor : "#E8E8E8" }`,
+       backgroundColor: disabled ? "#e9e9e9" : "white",
+       cursor: disabled ? "not-allowed" : "text",
+      }}
+      disabled={disabled}
+      maxLength={maxCount}
+      onChange={(e) => {
+        setInputValue(e.target.value);
+      }}
+      onFocus={() => setHover(true)}
+      onBlur={() => setHover(false)}
         {...props}
-      />
-      {characterCount && (
+        >
+        {inputValue}
+        </textarea>
+        {characterCount && (
         <span
           className="twizzle-input-text-character-count"
           style={{
             color: "#d5d5d5",
+            marginLeft:'auto',
+            marginTop:'5px',
+            backgroundColor: disabled ? "#e9e9e9" : "white",
           }}
         >
           {inputValue?.length}/{maxCount}
         </span>
       )}
+     
     </div>
   );
 };
