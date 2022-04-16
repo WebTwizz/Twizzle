6d0c02a641129586d115f9f6281ce33d2b66cc6b
ahmedwab <a.abdelfattah@vehikl.com>
2022-04-16 02:25:20 -0400

    Add Select Component

--- src/components/Input/Checkbox/Checkbox.tsx
+++ src/components/Input/Checkbox/Checkbox.tsx
@@ -1,42 +1,46 @@
-import React, { useState } from 'react';
-import './checkbox.css';
+import React, { useState } from "react";
+import "./checkbox.css";
 
 interface CheckboxProps {
-    label: string;
-    disabled?: boolean;
-    checked?: boolean;
-    onCheck?: (value: boolean) => void;
-    color?: string;
-    style?: React.CSSProperties;
+  label: string;
+  disabled?: boolean;
+  checked?: boolean;
+  onCheck?: (value: boolean) => void;
+  color?: string;
+  style?: React.CSSProperties;
 }
-const Checkbox:React.FC<CheckboxProps> = ({
-    disabled,
-    checked = false,
-    onCheck,
-    style,
+const Checkbox: React.FC<CheckboxProps> = ({
+  label,
+  disabled,
+  checked = false,
+  onCheck,
+  style,
 }) => {
-    const [isChecked, setIsChecked] = useState(checked);
-    const handleCheckboxChange = (e: React.MouseEvent<HTMLInputElement>) => {
-        setIsChecked(e.currentTarget.checked);
-        if (onCheck) {
-            onCheck(e.currentTarget.checked);
-        }
-    };
-    return (
-        <input type="checkbox" 
+  const [isChecked, setIsChecked] = useState(checked);
+  const handleCheckboxChange = (e: React.MouseEvent<HTMLInputElement>) => {
+    setIsChecked(e.currentTarget.checked);
+    if (onCheck) {
+      onCheck(e.currentTarget.checked);
+    }
+  };
+  return (
+    <div id="twizzle-checkbox-container" style={{display:"flex", alignItems:'center', padding:'10px'}}>
+      <input
+        type="checkbox"
         id="twizzle-checkbox"
         style={{
-            backgroundColor: 'yellow',
-            ...style,
+          backgroundColor: "yellow",
+          ...style,
         }}
         disabled={disabled}
         checked={isChecked}
-        onClick={(e) => {handleCheckboxChange(e)}}
-        />    
-    )
+        onClick={(e) => {
+          handleCheckboxChange(e);
+        }}
+      />
+      {label}
+    </div>
+  );
+};
 
-
-
-}
-
-export {Checkbox};
+export { Checkbox };
---
+++ src/components/Input/Select/Select.stories.tsx
@@ -0,0 +1,31 @@
+//create select component story
+import React, { useState } from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Select } from './Select';
+
+export default {
+    title: 'Components/Input/Select',
+    component: Select,
+} as ComponentMeta<typeof Select>;
+
+const options = [
+    'Option 1',
+    'Option 2',
+    'Option 3',
+    'Option 4'];
+export const Default: ComponentStory<typeof Select> = () => {
+    const [value, setValue] = useState(options[0]);
+    const changeSelect = (value: string) => {
+        setValue(value);
+    }
+    return (
+        <>
+            <h1>Value: {value}</h1>
+            <Select options={options} value={value} onSelect={(e) => changeSelect(e)} />
+        </>
+    )
+}
+export const Disabled: ComponentStory<typeof Select> = () => {
+   
+    return ( <> <Select disabled options={['option1', 'option2', 'option3']}/></> )
+}
