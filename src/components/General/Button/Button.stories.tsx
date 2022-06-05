c62357588b606cefc068c2d4457428e5188454ec
ahmedwab <a.abdelfattah@vehikl.com>
2022-06-04 21:36:41 -0400

    Update Button Component

--- src/components/General/Button/Button.stories.tsx
+++ src/components/General/Button/Button.stories.tsx
@@ -1,5 +1,4 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Loader } from '../../Feedback/Loader/Loader';
 import { Button } from './Button';
 
 const variants = [
@@ -12,30 +11,32 @@ const variants = [
 export default {
   title: 'Components/General/Button',
   component: Button,
+  argTypes: {
+    color: {
+      presetColors: ['red', 'green'],
+      control: { type: 'color' },
+    },
+    label: { control: 'text' },
+    outlined: { control: 'boolean' },
+    disabled: { control: 'boolean' },
+    corner:{
+      options: ['default', 'round'],
+      control: 'select',
+    },
+    variant:{
+      options: ['success' , 'info' , 'warning' , 'danger'],
+      control: 'select',
+    }
+  },
 } as ComponentMeta<typeof Button>;
 
-export const Default: ComponentStory<typeof Button> = () => <Button label={'Button'} onClick={(()=>alert('Button clicked!'))}/>;
-export const Outlined: ComponentStory<typeof Button> = () => <Button label={'Button'} outlined={true} onClick={(()=>alert('Button clicked!'))}/>;
-export const Disabled: ComponentStory<typeof Button> = () => <Button label={'Button'} disabled onClick={(()=>console.log('s'))}/>;
-export const Loading: ComponentStory<typeof Button> = () => <Button label={'Button'} loading={<Loader color={'white'}/>}/>;
-export const RoundedCorner: ComponentStory<typeof Button> = () => <Button label={'Button'} corner='round'/>;
 
-export const DefaultTypes: ComponentStory<typeof Button> = () => {
-  return (
-    <>
-    {variants.map(({type}) => {
-      return (
-        <Button label={type} variant={type} onClick={(()=>alert('Button clicked!'))}/>
-        
-      )
-    })}
-    <br/>
-    {variants.map(({type}) => {
-      return (
-        <Button label={type} variant={type} outlined onClick={(()=>alert('Button clicked!'))}/>
-        
-      )
-    })}
-    </>
-  )
-}
+const ButtonTemplate: ComponentStory<typeof Button> = ({ label, ...rest }) => {
+  
+  return <Button label={label} {...rest} />;
+};
+
+export const ButtonComponent = ButtonTemplate.bind({});
+ButtonComponent.args= {
+  label: 'Button',
+};
--- src/components/General/Button/Button.tsx
+++ src/components/General/Button/Button.tsx
@@ -1,23 +1,22 @@
-import React, { useContext, useState } from 'react'
-import { ThemeContext } from '../../../context/ThemeContext'
-import './button.css'
+import React, { useContext, useState } from "react";
+import { ThemeContext } from "../../../context/ThemeContext";
+import "./button.css";
 
 interface ButtonProps {
-  primary?: boolean;
-  backgroundColor?: string;
+  color?: string;
   label: string;
   outlined?: boolean;
   onClick?: () => void;
   disabled?: boolean;
   loading?: React.ReactNode;
   icon?: React.ReactNode;
-  corner?: 'default' | 'round';
-  variant?: 'success' | 'info' | 'warning' | 'danger'
+  corner?: "default" | "round";
+  variant?: "success" | "info" | "warning" | "danger";
   style?: React.CSSProperties;
 }
 
 const Button: React.FC<ButtonProps> = ({
-  backgroundColor,
+  color,
   label,
   outlined,
   style,
@@ -28,49 +27,56 @@ const Button: React.FC<ButtonProps> = ({
   variant,
   ...props
 }: ButtonProps) => {
-  const theme = useContext(ThemeContext)
+  const theme = useContext(ThemeContext);
   const [hover, setHover] = useState(false);
+
+  color =
+    theme?.variants?.[variant!]?.color ||
+    color ||
+    theme?.primary?.backgroundColor;
   return (
     <button
       type="button"
-      onMouseEnter={()=>{ setHover(true); }}
-      onMouseLeave={()=>{ setHover(false); }}
-      className={`${outlined ? 'twizzle-button-outlined' : 'twizzle-button'}${
-        disabled ? '-disabled' : ''
+      onMouseEnter={() => {
+        setHover(true);
+      }}
+      onMouseLeave={() => {
+        setHover(false);
+      }}
+      className={`${
+        disabled
+          ? "twizzle-button-disabled"
+          : outlined
+          ? "twizzle-button-outlined"
+          : "twizzle-button"
       }`}
       {...props}
       style={{
-        borderRadius: corner === 'round' ? '30px' : '5px',
+        borderRadius: corner === "round" ? "30px" : "5px",
         backgroundColor: disabled
-          ? '#EBEBE4'
+          ? "#EBEBE4"
           : outlined
-            ? 
-            hover
-            ? variant? 
-            theme?.variants?.[variant]?.color
-            :theme?.primary?.backgroundColor
-            :'white'
-            : variant? 
-            theme?.variants?.[variant]?.color
-            : 'theme?.primary?.backgroundColor',
-        color: 
-          outlined ? 
-          hover? 
-          'white' 
-          : variant? 
-            theme?.variants?.[variant]?.color
-            :theme?.primary?.backgroundColor 
-            : 'white',
-        filter: hover?'brightness(0.8)':'brightness(1)',
-        margin: '5px',
-        ...style
+          ? hover
+            ? color
+            : "white"
+          : color,
+        color: disabled
+          ? "white"
+          : outlined
+          ? hover
+            ? "white"
+            : color
+          : 'white',
+        filter: hover ? "brightness(0.8)" : "brightness(1)",
+        margin: "5px",
+        ...style,
       }}
       disabled={disabled}
     >
       <div
         style={{
-          display: 'inline-flex',
-          alignItems: 'center'
+          display: "inline-flex",
+          alignItems: "center",
         }}
       >
         {loading}
@@ -78,7 +84,7 @@ const Button: React.FC<ButtonProps> = ({
         {label}
       </div>
     </button>
-  )
-}
+  );
+};
 
-export { Button }
+export { Button };
