e9868d3d88ca8172bcf1d20764a0101b816129b1
ahmedwab <a.abdelfattah@vehikl.com>
2022-08-21 22:47:59 -0400

    Add Badge and Pill to exports

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ export * from './components/Input/Toggle';
 export * from './components/Layout/Alert';
 export * from './components/Layout/Modal';
 export * from './components/Layout/Tab';
+export * from './components/Layout/Badge';
 export * from './components/Navigation/Pagination';
 export * from './components/Other/BackToTop';
 export * from './components/Other/Tooltip';
